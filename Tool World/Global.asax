a2f09a56dcef697ee5ad9933e42428b8ad980d86
IkkoGa <ikkoharrison@gmail.com>
2020-10-07 12:54:05 -0400

    Add project files.

---
+++ Tool World/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tool_World.MvcApplication" Language="C#" %>
---
+++ Tool World/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tool_World.Startup))]
+namespace Tool_World
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
